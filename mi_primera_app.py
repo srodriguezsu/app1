a02e8a35fca36befb50d7e60d284be8651251fda
Sebastian Rodriguez Suarez <129565458+srodriguezsu@users.noreply.github.com>
2024-11-18 15:55:44 -0500

    Create mi_primera_app.py

---
+++ mi_primera_app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+# Título de la app
+st.title("Mi primera app")
+
+# Autor de la app
+st.write("Esta app fue elaborada por “Sebastian”.")  
+
+# Entrada del usuario
+nombre_usuario = st.text_input("Por favor, ingresa tu nombre:")
+
+# Saludo personalizado
+if nombre_usuario:
+    st.write(f"{nombre_usuario}, te doy la bienvenida a mi primera app.")
